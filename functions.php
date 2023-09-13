b53f12b0fd2b31a459cc19430618d29a0d127701
noravitkai <norvit01@easv365.dk>
2023-09-13 18:41:16 +0200

    Project setup

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+function mt_register_stylesheet() {
+    wp_enqueue_style('custom_style', get_template_directory_uri() . '/tailwind.css');
+}
+add_action('wp_enqueue_scripts', 'mt_register_stylesheet');
+
+function ad_remove_gutenberg() {
+    remove_post_type_support('post', 'editor');
+    remove_post_type_support('page', 'editor');
+}
+add_action('init', 'ad_remove_gutenberg');
+?>
---
+++ header.php
@@ -0,0 +1,4 @@
+<html>
+<body>
+<title><?php bloginfo("name") ?></title>
+<?php wp_head() ?>
