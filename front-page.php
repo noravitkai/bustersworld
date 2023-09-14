5c31bb950cc98e6c94bbb8add821ecf8b36ef2e9
noravitkai <norvit01@easv365.dk>
2023-09-14 17:06:24 +0200

    Added a Story Section to the Front Page.

--- front-page.php
+++ front-page.php
@@ -20,6 +20,42 @@ Template Name: Front Page
         </div>
     </section>
 
+    <!-- Story Section -->
+    <section class="story-section">
+        <div class="story-content">
+            <h2 class="story-heading"><?php the_field('story_heading'); ?></h2>
+
+            <div class="story-stages">
+                <div class="story-book">
+                    <?php $book_icon = get_field('book_icon'); ?>
+                    <?php if ($book_icon) : ?>
+                        <img src="<?php echo esc_url($book_icon['url']); ?>" alt="<?php echo esc_attr($book_icon['alt']); ?>">
+                    <?php endif; ?>
+                    <h3><?php the_field('book_description'); ?></h3>
+                    <a class="year-button" href="<?php the_field('book_link'); ?>"><?php the_field('book_year'); ?></a>
+                </div>
+
+                <div class="story-movie">
+                    <?php $movie_icon = get_field('movie_icon'); ?>
+                    <?php if ($movie_icon) : ?>
+                        <img src="<?php echo esc_url($movie_icon['url']); ?>" alt="<?php echo esc_attr($movie_icon['alt']); ?>">
+                    <?php endif; ?>
+                    <h3><?php the_field('movie_description'); ?></h3>
+                    <a class="year-button" href="<?php the_field('movie_link'); ?>"><?php the_field('movie_year'); ?></a>
+                </div>
+
+                <div class="story-play">
+                    <?php $play_icon = get_field('play_icon'); ?>
+                    <?php if ($play_icon) : ?>
+                        <img src="<?php echo esc_url($play_icon['url']); ?>" alt="<?php echo esc_attr($play_icon['alt']); ?>">
+                    <?php endif; ?>
+                    <h3><?php the_field('play_description'); ?></h3>
+                    <a class="year-button" href="<?php the_field('play_link'); ?>"><?php the_field('play_year'); ?></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <!-- Ticket Section -->
     <section class="ticket-section">
         <div class="ticket-content">
@@ -30,11 +66,17 @@ Template Name: Front Page
                     <?php if ($child_ticket_image) : ?>
                         <img src="<?php echo esc_url($child_ticket_image['url']); ?>" alt="<?php echo esc_attr($child_ticket_image['alt']); ?>">
                     <?php endif; ?>
-                <p><?php the_field('child_ticket_description') ?></p>
+                <div class="ticket-details">
+                    <p><?php the_field('child_ticket_description') ?></p>
+                    <a href="<?php the_field('ticket_button_link'); ?>" class="ticket-button"><?php the_field('ticket_button_text'); ?></a>
+                </div>
             </div>
 
             <div class="adult-ticket">
-                <p><?php the_field('adult_ticket_description') ?></p>
+                <div class="ticket-details">
+                    <p><?php the_field('adult_ticket_description') ?></p>
+                    <a href="<?php the_field('ticket_button_link'); ?>" class="ticket-button"><?php the_field('ticket_button_text'); ?></a>
+                </div>
                 <?php $adult_ticket_image = get_field('adult_ticket_image'); ?>
                     <?php if ($adult_ticket_image) : ?>
                         <img src="<?php echo esc_url($adult_ticket_image['url']); ?>" alt="<?php echo esc_attr($adult_ticket_image['alt']); ?>">
