0fea8fb280efa5441a28ea0c1f9ff4b47543c638
noravitkai <norvit01@easv365.dk>
2023-09-14 18:27:59 +0200

    Created a Navigation Bar.

--- header.php
+++ header.php
@@ -1,4 +1,22 @@
-<html>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Add this line -->
+    <title><?php bloginfo("name") ?></title>
+    <?php wp_head() ?>
+</head>
 <body>
-<title><?php bloginfo("name") ?></title>
-<?php wp_head() ?>
+<nav class="custom-navbar">
+    <div class="logo">
+        <?php $logo = get_field('logo'); ?>
+        <?php if ($logo) : ?>
+            <a href="<?php echo home_url(); ?>"><img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>"></a>
+        <?php endif; ?>
+    </div>
+    <ul class="menu">
+        <li><a href="<?php echo home_url(); ?>">Home</a></li>
+        <li><a href="<?php echo get_permalink(get_page_by_title('Programme')); ?>">Programme</a></li>
+    </ul>
+</nav>
+
